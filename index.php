97f14c8557131cd992129c8143c3249b04d7482d
frekike22 <60663408+frekike22@users.noreply.github.com>
2021-03-15 22:36:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Bootstrap 5 Crash Course</title>
+
+
+
+
+</head>
+<body>
+<div class="container" style="margin-bottom: 700px">
+<h1>Hello Bootstrap world</h1>
+
+<div class="container-md">
+    <div class="row">
+        <div class="col-sm">1</div>
+        <div class="col-sm">2</div>
+        <div class="col-sm">3</div>
+    </div>
+</div>
+
+<h1>Buttons</h1>
+
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+<hr>
+<h1>Cards</h1>
+<div class="card" style="width: 18rem">
+    <div class="card-body">
+        The most incredible card ever
+    </div>
+</div>
+
+
+
+
+
+
+
+</div>
+<script src="https://unpkg.com/@popperjs/core@2.4.0/dist/umd/popper.min.js"></script>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
